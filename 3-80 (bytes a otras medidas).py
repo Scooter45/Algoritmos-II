58307f51b51f9accb12a7ec550bed54b7c8d8b21
Scooter45 <101779289+Scooter45@users.noreply.github.com>
2022-08-12 14:36:42 -0500

    Add files via upload

---
+++ 3-80 (bytes a otras medidas).py
@@ -0,0 +1,13 @@
+B= int(input('Ingrese la cantidad de Bytes: '))
+KB = B * 1024
+MB = KB * 1024
+GB = MB * 1024
+TB = GB * 1024
+PB = TB * 1024
+
+print('La cantidad ingresada tiene las siguientes equivalencias:')
+print(f'{KB} kilobytes')
+print(f'{MB} megabytes')
+print(f'{GB} gigabytes')
+print(f'{TB} terabytes')
+print(f'{PB} petabytes')
