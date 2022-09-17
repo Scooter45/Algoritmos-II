2f6682f1a34a5a3d0ebbd8714b85beaee442396b
Scooter45 <101779289+Scooter45@users.noreply.github.com>
2022-09-17 14:18:40 -0500

    Delete 5-80 (calcular el iva de un producto).py

--- 5-80 (calcular el iva de un producto).py
+++
@@ -1,5 +0,0 @@
-precio = int(input('Ingrese el precio del producto al cual desea calcular el IVA: '))
-iva = ( precio / 100 ) * 19
-total = iva + precio
-print('el IVA del producto es :'f'{iva}')
-print('el valor total a pagar es :'f'{total}')
